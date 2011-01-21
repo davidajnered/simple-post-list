8849171ff5e817f5f0eeab757ccb9f40f8db2b08
davidajnered <david@goodold.se>
2011-01-21 15:28:44 +0100

    wrapped output in an ordered list

--- includes/view.php
+++ includes/view.php
@@ -4,29 +4,31 @@
   print $args['before_title'] . $title . $args['after_title'];
 } ?>
 
-<?php $id = 0; foreach($data as $post) : ?>
-  <li class="simple-post-list-post" id="simple-post-list-id-<?php print $id; ?>">
-    <h3><?php print $post->post_title; ?></h3>
-    <?php if($thumbnail == TRUE) : ?>
-      <a href="<?php print $url; ?>"><?php print get_the_post_thumbnail($post->ID, $thumbnail_size); ?></a>
-    <?php endif; ?>
+<ol>
+  <?php $id = 0; foreach($data as $post) : ?>
+    <li class="simple-post-list-post" id="simple-post-list-id-<?php print $id; ?>">
+      <h3><?php print $post->post_title; ?></h3>
+      <?php if($thumbnail == TRUE) : ?>
+        <a href="<?php print $url; ?>"><?php print get_the_post_thumbnail($post->ID, $thumbnail_size); ?></a>
+      <?php endif; ?>
 
-    <?php
-      $content = ($data_to_use == 'excerpt') ? strip_tags($post->post_excerpt) : strip_tags($post->post_content);
-      // Show the specified length of the content
-      if($length <= -1) {
-        $content = '';
-      } else if (strlen($content) > $length) {
-        if($length > 0) {
-          $content = substr($content, 0, $length) . '&hellip; ';
+      <?php
+        $content = ($data_to_use == 'excerpt') ? strip_tags($post->post_excerpt) : strip_tags($post->post_content);
+        // Show the specified length of the content
+        if($length <= -1) {
+          $content = '';
+        } else if (strlen($content) > $length) {
+          if($length > 0) {
+            $content = substr($content, 0, $length) . '&hellip; ';
+          }
         }
-      }
-    ?>
-    <p>
-      <?php print $content; ?>
-      <a href="<?php print get_bloginfo('url') . '?p=' . $post->ID; ?>"><?php print $link; ?></a>
-    </p>
-  </li>
-<?php $id++; endforeach; ?>
+      ?>
+      <p>
+        <?php print $content; ?>
+        <a href="<?php print get_bloginfo('url') . '?p=' . $post->ID; ?>"><?php print $link; ?></a>
+      </p>
+    </li>
+  <?php $id++; endforeach; ?>
+</ol>
 
 <?php print $args['after_widget']; ?>
